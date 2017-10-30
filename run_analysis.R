99a8d764e25ea8083e75b84cde1465db45e77f67
NilofarHassan <30449881+NilofarHassan@users.noreply.github.com>
2017-10-29 23:13:04 -0500

    removed library ref

--- run_analysis.R
+++ run_analysis.R
@@ -59,7 +59,7 @@ names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
 names(Data)
 
-library(plyr);
+
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
