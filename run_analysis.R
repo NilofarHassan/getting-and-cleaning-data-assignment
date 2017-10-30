28139932152b8cbd25c7bf3dcda16c29535e0300
NilofarHassan <30449881+NilofarHassan@users.noreply.github.com>
2017-10-29 23:01:18 -0500

    removed knitr ref

--- run_analysis.R
+++ run_analysis.R
@@ -64,7 +64,5 @@ Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
 
-library(knitr)
-knit2html("codebook.Rmd");
 
 
